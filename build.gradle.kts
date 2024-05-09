8f248caa4065cf712fe98c1092c66a84bafed18b
itssu <ssujinn96@gmail.com>
2024-05-09 15:45:14 +0900

    :heavy_plus_sign: swagger dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
